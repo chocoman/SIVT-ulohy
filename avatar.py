b49df57c7645f654a619d27b29721dc9ff848051
Martin Spanel <martin.spanel@newtontech.cz>
2020-11-12 09:33:53 +0100

    added random colors

--- avatar.py
+++ avatar.py
@@ -10,26 +10,33 @@ top = Tk()
 SIZE = 500
 C = Canvas(top, bg = "#000000", height = SIZE, width = SIZE)
 
+def intToHex(number):
+    hexadecimal = hex(number)[2:]
+    if len(hexadecimal) == 1:
+        return '0' + hexadecimal
+    else:
+        return hexadecimal
+
 def hexCodeFromRgb(red, green, blue):
-    hexRed = hex(red)[2:]
-    hexGreen = hex(green)[2:]
-    hexBlue = hex(blue)[2:]
+    hexRed = intToHex(red)
+    hexGreen = intToHex(green)
+    hexBlue = intToHex(blue)
     return f'#{hexRed}{hexGreen}{hexBlue}'
 
 def draw_symmetricLines(start_x, start_y, end_x, end_y, color):
-    line = C.create_line(start_x, start_y, end_x, end_y, fill=color)
-    line = C.create_line(SIZE - start_x, SIZE - start_y, SIZE - end_x, SIZE - end_y, fill=color)
-    line = C.create_line(SIZE - start_x, start_y, SIZE - end_x, end_y, fill=color)
-    line = C.create_line(start_x, SIZE - start_y, end_x, SIZE - end_y, fill=color)
+    C.create_line(start_x, start_y, end_x, end_y, fill=color)
+    C.create_line(SIZE - start_x, SIZE - start_y, SIZE - end_x, SIZE - end_y, fill=color)
+    C.create_line(SIZE - start_x, start_y, SIZE - end_x, end_y, fill=color)
+    C.create_line(start_x, SIZE - start_y, end_x, SIZE - end_y, fill=color)
 
-print(hexCodeFromRgb(0, 255, 0))
+
 for i in range(10):
     draw_symmetricLines(
         random.randint(0,500),
         random.randint(0,500),
         random.randint(0,500),
         random.randint(0,500),
-        'white',
+        hexCodeFromRgb(random.randint(0, 255), 0, 0),
     )
 C.pack()
 top.mainloop()
