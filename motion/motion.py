d537a61cd92b87360c2c3012ecf4fb351dae8a1e
Martin Spanel <spanel@arcig.cz>
2020-12-14 17:15:20 +0100

    implemented for segmentation

--- motion/motion.py
+++ motion/motion.py
@@ -20,21 +20,53 @@ def exportImage(numpy_image, output_path):
     pil_image = Image.fromarray(np.clip(np.uint8(numpy_image), 0, 255))
     pil_image.save(output_path)
 
+def getForegroundMask(frame, background):
+    differences = np.mean(np.abs(frame - background), 2)
+    foregroundMask = differences >= 15
+    return foregroundMask
+
 def removeBackground(frame, background):
     height, width, depth = frame.shape
-    newBackground = np.zeros((height, width, depth)) #np.flip(background, 1)
-    differences = np.mean(np.abs(frame - background), 2)
-    backgroundMask = differences < 20
+    newBackground = np.zeros((height, width, depth))
+    backgroundMask = getForegroundMask(frame, background) == False
     expandedMask = np.tile(np.reshape(backgroundMask, (height, width, 1)), (1, 1, 3))
     frame[expandedMask] = newBackground[expandedMask]
     return frame
+
+def markArea(i, j, areas, area_id, mask):
+    height, width = mask.shape
+    if i < 0 or j < 0 or i >= height or j >= width: return
+    if (mask[i, j] == False): return
+    if (areas[i, j] != 0): return
+    areas[i, j] = area_id
+    markArea(i, j - 1, areas, area_id, mask)
+    markArea(i - 1, j, areas, area_id, mask)
+    markArea(i, j + 1, areas, area_id, mask)
+    markArea(i + 1, j, areas, area_id, mask)
+
+def identifyAreas(mask):
+    height, width = mask.shape
+    areas = np.zeros(mask.shape)
+    area_id = 1
+    for i in range(height):
+      for j in range(width):
+        # do not search background.
+        if (mask[i, j] == False): continue
+        # do not search from already marked pixels.
+        if (areas[i, j] != 0): continue
+        markArea(i, j, areas, area_id, mask)
+        area_id += 1
+    return areas
 
 frames = loadSequence('coral/coral-', '.png', 55, 216, 384, 3)
 print(frames.shape)
 
 background = np.median(frames, 0)
 
+mask = getForegroundMask(frames[7], background)
+areas = identifyAreas(mask)
 
+exportImage((areas * 71) % 256 , './output/areas.png')
 exportImage(background, './output/background2.png')
 
 for i in range(len(frames)):
