303c34381cf7b9cc5b05511b2f591e88eed135d6
Martin Spanel <martin.spanel@newtontech.cz>
2021-04-21 16:16:04 +0200

    added basic filters

--- filter/filter.py
+++ filter/filter.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def loadImage(file_path):
     pil_image = Image.open(file_path)
-    numpy_image = np.asarray(pil_image)
+    numpy_image = np.asarray(pil_image, np.int32)
     #np.array(pic.getdata()).reshape(pic.size[0], pic.size[1], 3)
     print(f'loaded image, dimensions: {numpy_image.shape}')
     return numpy_image
@@ -12,9 +12,34 @@ def exportImage(numpy_image, output_path):
     pil_image = Image.fromarray(np.uint8(numpy_image))
     pil_image.show()
     pil_image.save(output_path)
-    
+
+def darken(image):
+    darkened = image - 50
+    darkened[darkened < 0] = 0
+    return darkened
+
+def darken2(image):
+    bw = (image[:,:,0] + image[:,:,1] + image[:,:,2])/3
+    bw[bw > 150] = bw[bw > 150] - 150
+    return bw
+
+def gradient(image):
+    darkening = 0
+    for i in range(image.shape[0]):
+        darkening = 200 * i / image.shape[0]
+        image[i] = image[i] - darkening
+    image[image < 0] = 0
+    return image
+
 image = loadImage('./ludmila.jpg')
-
-# TODO upravit obrazek
-
-exportImage(image, './output.jpg')
+#print(image)
+#black_and_white = (image[:,:,0] + image[:,:,1] + image[:,:,2])/3
+#rotated = np.rot90(image)
+#negative = 255-image
+#combined = np.concatenate((image, image), axis=1)
+#flipped = np.flip(image, axis=0)
+#cropped = image[:250,:,:]
+#dark = darken(image)
+#dark2 = darken2(image)
+grad = gradient(image)
+exportImage(grad, './output.jpg')
